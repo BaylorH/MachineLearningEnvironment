13e2c7bf0c029170ae8d22a7c4441e6419e20872
bmaggs1 <bmaggs@asu.edu>
2024-04-07 18:11:05 -0700

    FIxed cameramovement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -39,34 +39,21 @@ public class Movement : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftAlt))
+        if (Input.GetMouseButton(0))
         {
             var axis = Input.GetAxis("Mouse X");
             var axis2 = Input.GetAxis("Mouse Y");
-            if (Input.GetMouseButton(0))
-            {
-                _x += axis * _xSpeed * 0.03f;
-                _y += axis2 * _ySpeed * 0.03f;
-                _y = ClampAngle(_y, _yMinLimit, _yMaxLimit);
-            }
-            else if (Input.GetMouseButton(1))
-            {
-                var num = (Mathf.Abs(axis) <= Mathf.Abs(axis2)) ? axis2 : axis;
-                num = -num * _zoomSpeed * 0.03f;
-                _distance += num * (Mathf.Max(_distance, 0.02f) * 0.03f);
-            }
-            else if (Input.GetMouseButton(2))
-            {
-                var a = transform.rotation * Vector3.right;
-                var a2 = transform.rotation * Vector3.up;
-                var a3 = -a * axis * _xSpeed * 0.02f;
-                var b = -a2 * axis2 * _ySpeed * 0.02f;
-                _target += (a3 + b) * (Mathf.Max(_distance, 0.04f) * 0.01f);
-            }
+            _x += axis * _xSpeed * 0.03f;
+            _y += axis2 * _ySpeed * 0.03f;
+            _y = ClampAngle(_y, _yMinLimit, _yMaxLimit);
         }
-        else if (Input.GetKey(KeyCode.F))
+        else if (Input.GetMouseButton(1))
         {
-            _target = _startPos;
+            var axis = Input.GetAxis("Mouse X");
+            var axis2 = Input.GetAxis("Mouse Y");
+            var num = (Mathf.Abs(axis) <= Mathf.Abs(axis2)) ? axis2 : axis;
+            num = -num * _zoomSpeed * 0.03f;
+            _distance += num * (Mathf.Max(_distance, 0.02f) * 0.03f);
         }
         var rotation = Quaternion.Euler(_y, _x, 0f);
         var position = rotation * new Vector3(0f, 0f, -_distance) + _target;
