b55657e6e2150b2e16459fc894da833ce928a523
clarakim3 <kimyeonghyeon1706@gmail.com>
2024-04-09 12:27:45 -0700

    Adjust UI panel

--- Assets/PythonUnityScripts/PredictionRequester.cs
+++ Assets/PythonUnityScripts/PredictionRequester.cs
@@ -4,29 +4,23 @@ using NetMQ;
 using NetMQ.Sockets;
 using UnityEngine;
 
-public class PredictionRequester : RunAbleThread
-{
+public class PredictionRequester : RunAbleThread {
     private RequestSocket client;
 
     private Action<string> onOutputReceived;
     private Action<Exception> onFail;
     
-    protected override void Run()
-    {
+    protected override void Run() {
         ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
-        using (RequestSocket client = new RequestSocket())
-        {
+        using (RequestSocket client = new RequestSocket()) {
             this.client = client;
             client.Connect("tcp://localhost:5555");
 
-            while (Running)
-            {
+            while (Running) {
                 string output = null;
                 bool gotMessage = false;
-                while (Running)
-                {
-                    try
-                    {
+                while (Running) {
+                    try {
                         gotMessage = client.TryReceiveFrameString(out output); // this returns true if it's successful
                         if (gotMessage) break;
                     }
